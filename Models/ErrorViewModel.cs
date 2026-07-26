1667fdef6b4a527676a585be4b6df3140ba46b53
msejuro312 <msejuro312@protonmail.com>
2026-07-25 21:03:05 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T1_ManuelSejuro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
